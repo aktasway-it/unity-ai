501f7d6d236c6041c0f3460e08938143807d5e7e
Marco Rapino <aktasway@gmail.com>
2022-04-14 09:29:30 +0200

    - fixed namespaces in LookForwardBehavior.cs

--- Assets/Scripts/AI/Steering Behaviors/Behaviors/LookForwardBehavior.cs
+++ Assets/Scripts/AI/Steering Behaviors/Behaviors/LookForwardBehavior.cs
@@ -1,23 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using AI.Steering_Behaviors.Behaviors;
 using AI.Steering_Behaviors.Controllers;
 using UnityEngine;
 
-public class LookForwardBehavior : SteeringBehavior
+namespace AI.Steering_Behaviors.Behaviors
 {
-    public override SteeringData GetSteering(SteeringController controller)
+    public class LookForwardBehavior : SteeringBehavior
     {
-        if (controller.IsMoving)
+        public override SteeringData GetSteering(SteeringController controller)
         {
-            Quaternion lookAtRotation = Quaternion.LookRotation(controller.Velocity.normalized);
-            float yDelta = Mathf.Min(lookAtRotation.eulerAngles.y - controller.transform.eulerAngles.y, _acceleration);
-            Quaternion rotation = Quaternion.Euler(0, yDelta, 0);
-            _steeringData.angular = rotation.eulerAngles.y > 1 ? rotation : Quaternion.identity;
-        }
-        else
-            _steeringData.angular = Quaternion.identity;
+            if (controller.IsMoving)
+            {
+                Quaternion lookAtRotation = Quaternion.LookRotation(controller.Velocity.normalized);
+                float yDelta = Mathf.Min(lookAtRotation.eulerAngles.y - controller.transform.eulerAngles.y, _acceleration);
+                Quaternion rotation = Quaternion.Euler(0, yDelta, 0);
+                _steeringData.angular = rotation.eulerAngles.y > 1 ? rotation : Quaternion.identity;
+            }
+            else
+                _steeringData.angular = Quaternion.identity;
         
-        return _steeringData;
+            return _steeringData;
+        }
     }
 }
