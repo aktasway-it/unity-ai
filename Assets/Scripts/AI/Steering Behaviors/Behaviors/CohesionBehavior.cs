820ddbff433c2b48532f6fa8aaa401d6c23ca11c
Marco Rapino <aktasway@gmail.com>
2022-04-19 11:05:22 +0200

    - implemented cohesion and separation behaviors

---
+++ Assets/Scripts/AI/Steering Behaviors/Behaviors/CohesionBehavior.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using AI.Steering_Behaviors.Behaviors;
+using AI.Steering_Behaviors.Controllers;
+using AI.Steering_Behaviors.Data;
+using UnityEngine;
+
+public class CohesionBehavior : SteeringBehavior
+{
+    [SerializeField] private int _physicsMaxAgentCast = 10;
+    [SerializeField] private float _physicsAgentCastRadius = 2.0f;
+    [SerializeField] private float _minDistance = 1.0f;
+    [SerializeField] private float _decayCoefficient = 2.0f;
+    [SerializeField] private LayerMask _agentsLayer;
+    private Collider[] _agents;
+
+    public override void Init(SteeringController controller)
+    {
+        base.Init(controller);
+        _agents = new Collider[_physicsMaxAgentCast];
+    }
+
+    public override SteeringData GetSteering(SteeringController controller)
+    {
+        int agentsInRange =
+            Physics.OverlapSphereNonAlloc(controller.Position, _physicsAgentCastRadius, _agents, _agentsLayer);
+
+        _steeringData.linear = Vector3.zero;
+
+        for (int i = 0; i < agentsInRange; i++)
+        {
+            if (!_agents[i].CompareTag("AIAgentsGroup"))
+                continue;
+            
+            Vector3 direction = _agents[i].transform.position - controller.Position;
+            float distance = direction.magnitude;
+            if (distance > _minDistance)
+            {
+                float strength = Mathf.Min(_decayCoefficient / (distance * distance), _acceleration);
+                _steeringData.linear += strength * direction.normalized;
+            }
+        }
+
+        return _steeringData;
+    }
+}
--- Assets/Scripts/AI/Steering Behaviors/Behaviors/PursueBehavior.cs
+++ Assets/Scripts/AI/Steering Behaviors/Behaviors/PursueBehavior.cs
@@ -7,18 +7,27 @@ namespace AI.Steering_Behaviors.Behaviors
     public class PursueBehavior : TargetSteeringBehavior
     {
         [SerializeField] private float _maxPredictionTime = 3.0f;
+        [SerializeField] private float _minDistance = 1.5f;
+        
         public override SteeringData GetSteering(SteeringController controller)
         {
             Vector3 direction = _target.Position - controller.Position;
-            float distance = direction.magnitude;
-            float targetSpeed = _target.Velocity.magnitude;
+            if (direction.magnitude > _minDistance)
+            {
+                float distance = direction.magnitude;
+                float targetSpeed = _target.Velocity.magnitude;
 
-            float predictionTime = _maxPredictionTime;
-            if (targetSpeed > distance / _maxPredictionTime)
-                predictionTime = distance / targetSpeed;
+                float predictionTime = _maxPredictionTime;
+                if (targetSpeed > distance / _maxPredictionTime)
+                    predictionTime = distance / targetSpeed;
 
-            Vector3 futurePosition = _target.Position + _target.Direction * targetSpeed * predictionTime;
-            _steeringData.linear = (futurePosition - controller.Position) * _acceleration;
+                Vector3 futurePosition = _target.Position + _target.Direction * targetSpeed * predictionTime;
+                _steeringData.linear = (futurePosition - controller.Position) * _acceleration;
+            }
+            else
+            {
+                _steeringData.linear = Vector3.zero;
+            }
             return _steeringData;
         }
     }
